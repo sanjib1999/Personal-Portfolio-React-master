94a0c32f9c5507517ec30b983d16f5314dbddb62
Shubh404-SE <tanwarshubham29@gmail.com>
2023-10-13 19:18:12 +0530

    navbar responsive with mobile display size

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,66 +1,104 @@
-import React,{ useContext } from "react";
+import React, { useContext } from "react";
 import Toggle from "../Toggle/Toggle";
 import Logo from '../../image/logo.png'
 import "./Navbar.css";
 import { Link } from "react-scroll";
-import {themeContext} from "../../Context";
+import { themeContext } from "../../Context";
+import { useState } from "react";
+import { VscChromeClose } from 'react-icons/vsc';
+import { SlMenu } from 'react-icons/sl';
 
 const Navbar = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
-  
-  
+
+
+  const [mobileMenu, setmobileMenu] = useState();
+  const [show, setshow] = useState("hide");
+
+
+  window.addEventListener('scroll', (event) => {
+    let navbar = document.getElementsByClassName("navbar");
+
+    if (window.pageYOffset !== undefined) {
+      if (window.pageYOffset > 25) {
+        navbar[0].style.background = "aliceblue";
+      }
+      else {
+        navbar[0].style.background = "transparent";
+      }
+    }
+  })
+
+  const openMobileMenu = () => {
+    setmobileMenu(true);
+    setshow("show");
+  }
+
+  const closemobileMenue = () => {
+    setmobileMenu(false);
+    setshow("hide");
+  }
+  const navigatepage = () => { // to move top when click on link and hide the mobile menue
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+    setmobileMenu(false);
+    setshow("hide");
+  }
+
+
+
   return (
-    <div className="n-wrapper" id="Navbar" style={{backgroundColor:darkMode?"black":"white"}}>
+    <div className='navbar' style={{ backgroundColor: darkMode ? "black" : "white" }}>
       {/* left */}
       <div className="n-left">
-        <img src={Logo} alt="" style={{"height":"65px","width":"65px", marginTop:"0.6rem",marginLeft:"0.6rem"}} />
-        <div className="n-name" style={{color: darkMode ? "white" : "rgb(0,0,153)",fontFamily: "Arial,sans-serif"}}>EXAMIRROR</div>
+        <img src={Logo} alt="" style={{ "height": "65px", "width": "65px" }} />
+        <div className="n-name" style={{ color: darkMode ? "white" : "rgb(0,0,153)", fontFamily: "Arial,sans-serif" }}>EXAMIRROR</div>
       </div>
+
+      <div className={`linkouter ${show}`} onClick={closemobileMenue}>  </div>
+
       {/* right */}
-      <div className="n-right">
-        <div className="n-list">
-          <ul style={{ listStyleType: "none" }}>
-            <li>
-              <Link activeClass="active" to="Intro" spy={true} smooth={true}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="services" spy={true} smooth={true}>
-                Features
-              </Link>
-            </li>
-            <li>
-              <Link to="works" spy={true} smooth={true}>
-                Experience
-              </Link>
-            </li>
-            <li>
-              <Link to="portfolio" spy={true} smooth={true}>
-                Portfolio
-              </Link>
-            </li>
-            <li>
-              <Link to="testimonial" spy={true} smooth={true}>
-                Testimonial
-              </Link>
-            </li>
-            <li>
-              <Link to="contact" spy={true} smooth={true}>
-                <button className="button n-button">Contact</button>
-              </Link>
-            </li>
+      <div className={`navlinks ${show}`} style={{ backgroundColor: darkMode ? "black" : "white" }}>
 
-            <li>
-              <Toggle />
-            </li>
-          </ul>
+        <Link className="link" onClick={navigatepage}  activeClass="active" to="Intro" spy={true} smooth={true}>
+          Home
+        </Link>
+
+        <Link className="link" onClick={navigatepage}  to="services" spy={true} smooth={true}>
+          Features
+        </Link>
+
+        <Link className="link" onClick={navigatepage}  to="works" spy={true} smooth={true}>
+          Experience
+        </Link>
+
+        <Link className="link" onClick={navigatepage}  to="portfolio" spy={true} smooth={true}>
+          Portfolio
+        </Link>
+
+        <Link className="link" onClick={navigatepage}  to="testimonial" spy={true} smooth={true}>
+          Testimonial
+        </Link>
+
+        <Link className="link" onClick={navigatepage}  to="contact" spy={true} smooth={true}>
+          <button className="button n-button">Contact</button>
+        </Link>
+
+        <div className="link" >
+          <Toggle />
         </div>
-        
-
-        
       </div>
+
+      {/* mobile menu */}
+      {mobileMenu ? (
+        <VscChromeClose id='menu__toggle' onClick={closemobileMenue} />
+      ) : (
+        <SlMenu id='menu__toggle' onClick={openMobileMenu} />
+      )}
     </div>
   );
 };
